837b270313c5632c35b72c78c9f3d929e143c91d
adeng21 <adeng21@gmail.com>
2014-04-02 17:48:07 -0400

    blank rails template

---
+++ dive_in/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DiveIn::Application.config.secret_key_base = 'cba4b76f4608bb18bfb15ecaadb8f02c3b601c7af496dc81229a5ad207d0061a5d4d7653a910284f6a0c51af4d0f6c68b866ad329f1e41b10b9ad69ab4cea4b6'
