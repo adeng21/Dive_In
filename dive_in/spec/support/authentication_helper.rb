b95d8238401a082e41cfdcc83a2e7c1c859b46f9
adeng21 <adeng21@gmail.com>
2014-04-02 20:33:04 -0400

    added user sign in functionality

--- dive_in/spec/support/authentication_helper.rb
+++ dive_in/spec/support/authentication_helper.rb
@@ -4,7 +4,10 @@ module AuthenticationHelper
     visit new_user_session_path
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
-    click_on 'Sign in'
+
+    within('form') do
+      click_on 'Sign in'
+    end
   end
 
 end
