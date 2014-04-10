4e86e0e83f38b2819372f233e5f279e1d5942625
adeng21 <adeng21@gmail.com>
2014-04-10 14:08:19 -0400

    Added uniqueness index to months

--- dive_in/db/schema.rb
+++ dive_in/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140410180026) do
+ActiveRecord::Schema.define(version: 20140410180545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,8 @@ ActiveRecord::Schema.define(version: 20140410180026) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "months", ["name"], name: "index_months_on_name", unique: true, using: :btree
 
   create_table "reviews", force: true do |t|
     t.string   "title",       null: false
