2ee5308884e40be7f10ed685488e6613577a2712
adeng21 <adeng21@gmail.com>
2014-04-16 12:12:24 -0400

    Added map to homepage

--- dive_in/app/controllers/home_controller.rb
+++ dive_in/app/controllers/home_controller.rb
@@ -1,8 +1,16 @@
 class HomeController < ApplicationController
 
+
   def index
     @categories = Category.all
     @months = Month.all
+    @divesites = Divesite.all
+    @hash = Gmaps4rails.build_markers(@divesites) do |divesite, marker|
+      marker.lat divesite.latitude
+      marker.lng divesite.longitude
+      marker.infowindow divesite.name
+      marker.json({id: divesite.id})
+    end
   end
 
 end
