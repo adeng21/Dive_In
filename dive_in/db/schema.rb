7a7957577bb0ed90c12e22c0d99182183320c957
adeng21 <adeng21@gmail.com>
2014-04-22 17:27:07 -0400

    added image column to categories and an image file for each category

--- dive_in/db/schema.rb
+++ dive_in/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140416194300) do
+ActiveRecord::Schema.define(version: 20140422195122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140416194300) do
     t.string   "name",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
   add_index "categories", ["name"], name: "index_categories_on_name", unique: true, using: :btree
