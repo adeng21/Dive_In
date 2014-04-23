4aa199a8baed9bd276b585e060c267cccaabd56b
adeng21 <adeng21@gmail.com>
2014-04-23 11:22:47 -0400

    changed images for categories

--- dive_in/app/assets/javascripts/maps/about_page.js
+++ dive_in/app/assets/javascripts/maps/about_page.js
@@ -3,7 +3,7 @@ var boxChildren = document.getElementById('about').getElementsByTagName('p');
 
 $(document).ready(function(){
   $(boxChildren).hide().each(function(i){
-    $(this).delay(3000* i).fadeIn();
+    $(this).delay(4000* i).fadeIn('slow');
   });
 });
 
