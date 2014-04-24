184f962b1979f1cc6057be134422c0b2414fb56a
adeng21 <adeng21@gmail.com>
2014-04-24 14:15:53 -0400

    trying to fix hover feature

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -6,5 +6,5 @@ function changeImage(event){
   $target = $(event.target);
   $imageUrl = $target.attr('data-image');
   url = '/categories' + $imageUrl.slice($imageUrl.lastIndexOf('/'));
-  $('#category').css('background-image', 'url("assets/'+ url + '")');
+  $('#category').css('background-image', 'url("assets'+ url + '")');
 }
