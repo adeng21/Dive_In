d64aeae7d9d691388e0dee3c2ec2a66781833728
adeng21 <adeng21@gmail.com>
2014-04-24 14:10:30 -0400

    update javscripts

--- app/assets/javascripts/about_page.js
+++ app/assets/javascripts/about_page.js
@@ -1,11 +1,9 @@
-var boxChildren = document.getElementById('about').getElementsByTagName('p');
-
-
-$(document).ready(function(){
+function slowText(){
+  var boxChildren = document.getElementById('about').getElementsByTagName('p');
   $(boxChildren).hide().each(function(i){
     $(this).delay(2000* i).fadeIn('slow');
   });
-});
+};
 
 
 
