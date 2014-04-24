e0bad0c104078cf22951b4988b831023d30ea73a
adeng21 <adeng21@gmail.com>
2014-04-24 11:35:11 -0400

    changed about page transition time

--- dive_in/app/assets/javascripts/maps/about_page.js
+++ dive_in/app/assets/javascripts/maps/about_page.js
@@ -3,7 +3,7 @@ var boxChildren = document.getElementById('about').getElementsByTagName('p');
 
 $(document).ready(function(){
   $(boxChildren).hide().each(function(i){
-    $(this).delay(4000* i).fadeIn('slow');
+    $(this).delay(2000* i).fadeIn('slow');
   });
 });
 
