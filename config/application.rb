bdc4d32b54999f4ad172232d0f105e6b2e20a326
adeng21 <adeng21@gmail.com>
2014-04-25 11:48:09 -0400

    fix operator

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,6 @@ module DiveIn
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.assets.precompile += "assets/images"
+    config.assets.precompile << "assets/images"
   end
 end
